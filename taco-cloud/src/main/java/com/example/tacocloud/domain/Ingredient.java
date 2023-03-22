4d025c992974ed304f808c03c999020cbf72943b
kimhyonseong <corea6524@gmail.com>
2023-03-22 18:02:03 +0900

    create taco-cloud project

---
+++ taco-cloud/src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -0,0 +1,18 @@
+package com.example.tacocloud.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor  //final 또는 @NotNull 변수에 대한 생성자 생성
+@AllArgsConstructor  //전체를 매개변수로한 생성자 생성
+public class Ingredient {
+  private final String id;
+  private final String name;
+  private final Type type;
+
+  public static enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
+}
