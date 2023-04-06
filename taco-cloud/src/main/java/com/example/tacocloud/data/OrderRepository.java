9498082c95994172303921b52428ee9a3ea700b1
kimhyonseong <corea6524@gmail.com>
2023-04-06 17:49:34 +0900

    feat: use jpa

--- taco-cloud/src/main/java/com/example/tacocloud/data/OrderRepository.java
+++ taco-cloud/src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -3,5 +3,10 @@ package com.example.tacocloud.data;
 import com.example.tacocloud.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order,Long> {
+  List<Order> findByDeliveryZip(String deliveryZip);
+  List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
