6b08c06c373a2b5fab1ea9fe397c2b85b38786ba
kimhyonseong <corea6524@gmail.com>
2023-04-12 18:06:00 +0900

    fix: h2-console error / add user by jdbc

--- taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,28 +1,59 @@
 package com.example.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
   @Bean
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+            .build();
+  }
+
+//  @Bean
+//  public WebSecurityCustomizer webSecurityCustomizer() {
+//    return web -> web.ignoring().requestMatchers("/h2-console/**","/h2-console");
+//  }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests()
+    http
+            .authorizeHttpRequests()
             .requestMatchers("/design","/orders").hasRole("USER")
             .requestMatchers("/","/images/**").permitAll()
-            .and().httpBasic();
+            .requestMatchers(PathRequest.toH2Console()).permitAll()  // h2 콘솔
+            .requestMatchers("/h2-console/**").permitAll()  // h2 콘솔
+            .and()
+            .csrf().disable()
+            .headers().frameOptions().disable().and()  //iframe 나오게 하기
+            .httpBasic();
     return http.build();
   }
 
+  /*
   @Bean
   public InMemoryUserDetailsManager userDetailsManager() {
     PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -39,5 +70,18 @@ public class SecurityConfig {
             .roles("USER").build();
 
     return new InMemoryUserDetailsManager(user1,user2);
+  }
+  */
+  @Bean
+  public UserDetailsManager userDetailsManager(DataSource dataSource) {
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    UserDetails user1 = User.builder()
+            .username("user1")
+            .password(passwordEncoder.encode("password1"))
+            .roles("USER").build();
+    JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    users.createUser(user1);
+    return users;
   }
 }
