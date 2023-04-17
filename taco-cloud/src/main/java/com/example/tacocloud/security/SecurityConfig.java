bbf8a4646a52b61b2a5785f4d7a3f1f905eac531
kimhyonseong <corea6524@gmail.com>
2023-04-17 23:53:42 +0900

    feat: custom login

--- taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,24 +1,20 @@
 package com.example.tacocloud.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -43,52 +39,29 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
             .authorizeHttpRequests()
-            .requestMatchers("/design","/orders").hasRole("USER")
+            .requestMatchers("/design","/orders")
+            .access(AuthorityAuthorizationManager.hasRole("USER"))
+            //.hasRole("USER")
             .requestMatchers(PathRequest.toH2Console()).permitAll()  // h2 콘솔
             .requestMatchers("/","/**").permitAll()
             .and()
-            .csrf().disable()
             .headers().frameOptions().disable().and()  //iframe 나오게 하기
-            .httpBasic();
+            .httpBasic()
+            .and()
+            .formLogin().loginPage("/login")  // 커스텀 로그인 페이지
+            //.loginProcessingUrl("/authenticate")
+            .usernameParameter("user")
+            .passwordParameter("pwd")
+            .defaultSuccessUrl("/design")
+            .and()
+            .logout()
+            .logoutSuccessUrl("/")
+            .and()
+            .csrf()
+            .disable()
+    ;
     return http.build();
   }
-
-//  @Autowired
-//  public void ldapConfigure(AuthenticationManagerBuilder auth) throws Exception {
-//    auth.ldapAuthentication()
-//            .userSearchBase("ou=people")
-//            .userSearchFilter("(uid={0})")
-//            .groupSearchBase("ou=groups")
-//            .groupSearchFilter("member={0}")
-//            .contextSource()
-////            .root("dc=tacocloud,dc=com")
-////            .ldif("classpath:users.ldif")
-////            .url("ldap://localhost:8389/dc=tacocloud,dc=com")
-//            .and()
-//            .passwordCompare()
-//            .passwordEncoder(new BCryptPasswordEncoder())
-//            .passwordAttribute("userPasscode");
-//  }
-
-  /*
-  @Bean
-  public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
-    EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
-            EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
-    contextSourceFactoryBean.setPort(8389);
-    return contextSourceFactoryBean;
-  }
-
-  @Bean
-  AuthenticationManager ldapAuthenticationManager(
-          BaseLdapPathContextSource contextSource) {
-    LdapBindAuthenticationManagerFactory factory =
-            new LdapBindAuthenticationManagerFactory(contextSource);
-    factory.setUserDnPatterns("uid={0},ou=people");
-    factory.setUserDetailsContextMapper(new PersonContextMapper());
-    return factory.createAuthenticationManager();
-  }
-   */
 
   @Bean
   public AuthenticationManager userDetailsManager(HttpSecurity security, UserDetailsService userDetailsService) throws Exception{
