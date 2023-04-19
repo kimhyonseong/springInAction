ceac15331838858d304cbbd2b3097033e696564b
kimhyonseong <corea6524@gmail.com>
2023-04-19 20:19:42 +0900

    add yml

--- taco-cloud/src/main/java/com/example/tacocloud/controller/OrderController.java
+++ taco-cloud/src/main/java/com/example/tacocloud/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.example.tacocloud.controller;
 import com.example.tacocloud.data.OrderRepository;
 import com.example.tacocloud.domain.Order;
 import com.example.tacocloud.domain.Users;
+import com.example.tacocloud.property.OrderProps;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +21,12 @@ import org.springframework.web.bind.support.SessionStatus;
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
-
+  private OrderProps orderProps;
   private OrderRepository orderRepository;
 
-  public OrderController(OrderRepository orderRepository) {
+  public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
     this.orderRepository = orderRepository;
+    this.orderProps = orderProps;
   }
 
   @GetMapping("/current")
@@ -58,5 +63,11 @@ public class OrderController {
     orderRepository.save(order);
     sessionStatus.setComplete();
     return "redirect:/";
+  }
+
+  @GetMapping
+  public String ordersForUser(@AuthenticationPrincipal Users users, Model model) {
+    model.addAttribute("orders",orderRepository.findByUsersOrderByPlacedAtDesc(users, PageRequest.of(0,orderProps.getPageSize())));
+    return "orderList";
   }
 }
--- taco-cloud/src/main/java/com/example/tacocloud/data/OrderRepository.java
+++ taco-cloud/src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.tacocloud.data;
 
 import com.example.tacocloud.domain.Order;
+import com.example.tacocloud.domain.Users;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
@@ -9,4 +11,5 @@ import java.util.List;
 public interface OrderRepository extends CrudRepository<Order,Long> {
   List<Order> findByDeliveryZip(String deliveryZip);
   List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+  List<Order> findByUsersOrderByPlacedAtDesc(Users users, Pageable pageable);
 }
